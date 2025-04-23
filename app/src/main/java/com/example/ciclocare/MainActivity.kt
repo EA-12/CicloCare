50a44b2ada973f70f2efd2b42af0aaec15b84492
E-12 <evalcaide@gmail.com>
2025-04-23 16:23:46 +0200

    feat(views): añade pantallas de login, registro y perfil

--- app/src/main/java/com/example/ciclocare/MainActivity.kt
+++ app/src/main/java/com/example/ciclocare/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -16,18 +14,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.ciclocare.ui.components.BottomNavBar
 import com.example.ciclocare.ui.constants.NavItemList
 import com.example.ciclocare.ui.theme.CicloCareTheme
 import com.example.ciclocare.ui.view.Consejos
 import com.example.ciclocare.ui.view.Cuestionarios
 import com.example.ciclocare.ui.view.Home
+import com.example.ciclocare.ui.view.LogIn
 import com.example.ciclocare.ui.view.Monitorizacion
-
+import com.example.ciclocare.ui.view.Perfil
+import com.example.ciclocare.ui.view.Registro
 
  class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -52,7 +48,19 @@ fun App() {
      composable("cuestionarios") { Cuestionarios(navController) }
     }
     */
-    BottomNavScreen()
+    val screen = 3
+    if (screen == 0) {
+        LogIn()
+    }
+    if (screen == 1) {
+        Registro()
+    }
+    if (screen == 2) {
+        Perfil()
+    }
+    if (screen == 3) {
+        BottomNavScreen()
+    }
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
